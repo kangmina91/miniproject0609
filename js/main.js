baf5806d39e503bbf593b9ebaca336ee6551b061
kangmina91 <133613566+kangmina91@users.noreply.github.com>
2023-06-09 14:11:58 +0900

    5주차 과제입니다
    5주차 과제입니다

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+
+    $('.main_slide').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true,
+        speed: 1500,
+
+    });
+
+
+})
